113e6336bef8247873053e55b9347f650462205b
Aaron <83556435+happyloa@users.noreply.github.com>
2025-06-08 15:00:22 +0800

    Add page title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+  <title>高雄市旅遊資訊網 | Kaohsiung City Travel Info</title>
+  <meta name="description" content="提供高雄市各區景點資訊、開放時間、地址、電話及票價查詢，並可依區域篩選和瀏覽熱門景點。" />
+</svelte:head>
+
 <slot />
